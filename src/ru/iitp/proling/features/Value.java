dd5b13ee02c33b31a596a63360e5f7bc813cd190
Anton Kazennikov <kazennikov@gmail.com>
2011-08-05 13:43:57 +0400

    added javadoc

--- src/ru/iitp/proling/features/Value.java
+++ src/ru/iitp/proling/features/Value.java
@@ -1,10 +1,38 @@
 package ru.iitp.proling.features;
 
+/**
+ * Generic object to store some value
+ * @author Anton Kazennikov
+ *
+ */
 public interface Value {
+	/**
+	 * Get actual value object
+	 */
 	public Object get();
+	
+	/**
+	 * Set actual value object
+	 * @param o
+	 */
 	public void set(Object o);
+	
+	/**
+	 * Checks if value object is null
+	 * @return
+	 */
 	public boolean isNull();
+	
+	/**
+	 * Clears value
+	 */
 	public void clear();
+	
+	/**
+	 * Write string representation of this value to StringBuilder
+	 * @param sb string builder
+	 * @return sb
+	 */
 	public StringBuilder toStringBuilder(StringBuilder sb);
 
 }
