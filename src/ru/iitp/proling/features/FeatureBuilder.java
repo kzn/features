5871121d9f09056c3332e1788f46e4191754b3f8
Anton Kazennikov <kazennikov@gmail.com>
2011-08-05 14:21:18 +0400

    added feature builder

---
+++ src/ru/iitp/proling/features/FeatureBuilder.java
@@ -0,0 +1,11 @@
+package ru.iitp.proling.features;
+
+/**
+ * Feature builder interface.
+ * @author Anton Kazennikov
+ *
+ */
+public interface FeatureBuilder {
+	public Feature build(Value[] args);
+
+}
