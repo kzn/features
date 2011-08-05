79ac072cc4be153308960c872d730ed9e03b8847
Anton Kazennikov <kazennikov@gmail.com>
2011-08-05 13:41:58 +0400

    Added javadoc

--- src/ru/iitp/proling/features/Feature.java
+++ src/ru/iitp/proling/features/Feature.java
@@ -23,8 +23,27 @@ public interface Feature {
 	public interface Injectable {
 	}
 	
+	/**
+	 * Get feature function name
+	 * @return
+	 */
 	public String name();
+	
+	/**
+	 * Return feature function arguments
+	 */
 	public List<Value> args();
+	
+	/**
+	 * Evaluate feature and store result in a value object
+	 * @param res result value object
+	 */
 	public void eval(Value res);
+	
+	/**
+	 * Write feature function complete name to StringBuilder
+	 * @param sb target string builder
+	 * @return sb object
+	 */
 	public StringBuilder toStringBuilder(StringBuilder sb);
 }
