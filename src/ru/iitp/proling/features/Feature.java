c27dcd2455bd0476bec33e7777a04d0e141ba23f
Anton Kazennikov <kazennikov@gmail.com>
2011-08-05 13:37:20 +0400

    added javadoc to Feature.Injectable

--- src/ru/iitp/proling/features/Feature.java
+++ src/ru/iitp/proling/features/Feature.java
@@ -8,6 +8,18 @@ import java.util.List;
  *
  */
 public interface Feature {
+	/**
+	 * This interface indicates that the first element of the feature
+	 * is an injected root element.
+	 * 
+	 * <p>
+	 * Suppose, there is a w(n) feature, that returns word from a sentence at 
+	 * position n. So, actually, this feature is a w(root, n) feature, if
+	 * sentence is a root object.
+	 * This interface permits to write w(n) instead of w(root, n)
+	 * @author Anton Kazennikov
+	 *
+	 */
 	public interface Injectable {
 	}
 	
