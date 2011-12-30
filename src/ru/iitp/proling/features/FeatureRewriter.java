9eca3b0b0d3bd160d73c92016445ecaa89a184af
Anton Kazennikov <kazennikov@gmail.com>
2011-12-30 17:24:58 +0300

    added poly rewriter. rewrites combinatorial expressions

--- src/ru/iitp/proling/features/FeatureRewriter.java
+++ src/ru/iitp/proling/features/FeatureRewriter.java
@@ -7,11 +7,30 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 
+import ru.iitp.proling.features.FeatureParser.Special;
+
+/**
+ * AST Feature rewriter
+ * @author Anton Kazennikov
+ *
+ */
 public interface FeatureRewriter {
+	/**
+	 * Rewrites feature f to new feature
+	 * @param f feature to rewrite
+	 * @return rewrited feature
+	 */
 	public Feature rewrite(Feature f);
 	
-	
+	/*
+	 * Abstract class for rewriting features recursively
+	 */
 	public static abstract class Common implements FeatureRewriter {
+		/**
+		 * Rewrite feature arguments
+		 * @param f feature to rewrite
+		 * @return same feature function with rewritten arguments
+		 */
 		public Feature rewriteArgs(Feature f) {
 			List<Value> args = new ArrayList<Value>();
 			
@@ -23,12 +42,22 @@ public interface FeatureRewriter {
 			return f;
 		}
 		
+		/**
+		 * Rewrites a value
+		 * @param v source value
+		 * @return rewritten value
+		 */
 		public Value rewrite(Value v) {
 			if(v instanceof Feature)
 				return rewrite((Feature)v);
 			return v;
 		}
 		
+		/**
+		 * Rewrites list of values
+		 * @param v list of values to rewrite
+		 * @return rewritten list of values
+		 */
 		public List<Value> rewrite(List<Value> v) {
 			List<Value> r = new ArrayList<Value>(v.size());
 			for(Value val : v) {
@@ -39,6 +68,12 @@ public interface FeatureRewriter {
 		}
 	}
 	
+	/**
+	 * Root injector rewriter. Rewrites injectable functions as:
+	 * f(x1...xN) -> f(root, x1...xN)
+	 * @author Anton Kazennikov
+	 *
+	 */
 	public static class RootInjector implements FeatureRewriter {
 		FeatureExtractor eval;
 		
@@ -68,7 +103,7 @@ public interface FeatureRewriter {
 	}
 	
 	/**
-	 * f(i,j,k) -> tuple(f(i), f(j), f(k)) rewriter.
+	 * f(i,j,k) -> tuple(f(i), f(j), f(k)) rewriter for Indexed features.
 	 * Works on already root injected features
 	 * @author Anton Kazennikov
 	 *
@@ -114,6 +149,11 @@ public interface FeatureRewriter {
 		}
 	}
 	
+	/**
+	 * Minimized the AST to unique instances of each feature
+	 * @author Anton Kazennikov
+	 *
+	 */
 	public static class MinimizeRewriter implements FeatureRewriter {
 		FeatureExtractor eval;
 		
@@ -144,7 +184,10 @@ public interface FeatureRewriter {
 
 		@Override
 		public Feature rewrite(Feature f) {
+			
 			List<Value> args = new ArrayList<Value>();
+
+			// expand sequence to the upper level
 			for(Value arg : f.args()) {
 				if(arg instanceof Feature) {
 					Feature feat = rewrite((Feature)arg);
@@ -156,7 +199,7 @@ public interface FeatureRewriter {
 					args.add(arg);
 				}
 			}
-			
+			// if the Feature is Special
 			if(f instanceof FeatureParser.Special) {
 				List<Value> feats = new ArrayList<Value>();
 				Value base = args.get(0);
@@ -173,6 +216,10 @@ public interface FeatureRewriter {
 		}		
 	}
 	
+	/**
+	 * Expand inner sequences to the main sequence.
+	 * For example: (seq a b c (seq d e f)) -> (seq a b c d e f)
+	 */
 	public static class SequenceRewriter implements FeatureRewriter {
 
 		@Override
@@ -203,6 +250,11 @@ public interface FeatureRewriter {
 		}
 	}
 	
+	/**
+	 * Tuple simplifier. Rewrites (tuple a b (tuple c d)) -> (tuple a b c d)
+	 * @author Anton Kazennikov
+	 *
+	 */
 	public static class TupleSimplifier implements FeatureRewriter {
 
 		@Override
@@ -214,6 +266,7 @@ public interface FeatureRewriter {
 				Value v = q.pollFirst();
 				if(v instanceof Feature) {
 					Feature feat = (Feature)v;
+					
 					if(feat.getFeature() == CommonFeatures.tuple && f.getFeature() == CommonFeatures.tuple) {
 						List<Value> seqArgs = new ArrayList<Value>(feat.args());
 						Collections.reverse(seqArgs);
@@ -268,6 +321,87 @@ public interface FeatureRewriter {
 	
 			return rewriteArgs(f);
 		}
+	}
+	
+	/**
+	 * poly-features rewrites (poly 2 a b c d) -> (seq ab ac ad bc bd cd)
+	 * @author Anton Kazennikov
+	 *
+	 */
+	public static class PolyRewriter extends Common {
+		
+		public void rewritePoly(FeatureParser.Special poly, List<Value> args) {
+			int degree = poly.arg(0).get(Integer.class);
+			Deque<Value> deque = new ArrayDeque<Value>();
+			rewritePoly(degree, 1, poly.args(), args, deque);
+		}
+		
+		/**
+		 * Extender poly rewriter poly(n, a, b, c) = poly(1, a,b,c), poly(2, a,b,c) ... poly(n, a, b, c)
+		 * @param poly
+		 * @param args
+		 */
+		public void rewritePolyExtended(FeatureParser.Special poly, List<Value> args) {
+			int degree = poly.arg(0).get(Integer.class);
+			Deque<Value> deque = new ArrayDeque<Value>();
+			for(int i = 1; i != degree + 1; i++) {
+				rewritePoly(degree, 1, poly.args(), args, deque);
+			}
+
+			
+		}
+		
+		/**
+		 * Actual rewrite function
+		 * @param degree poly degree
+		 * @param startIdx start index for combination
+		 * @param args source arguments for combination
+		 * @param dest destination arguments
+		 * @param queue queue for gathering current combination of values
+		 */
+		public void rewritePoly(int degree, int startIdx, List<Value> args, List<Value> dest, Deque<Value> queue) {
+			
+			for(int i = startIdx; i < args.size(); i++) {
+				queue.addLast(args.get(i));
+
+				if(degree < 2) {				
+					dest.add(new Feature(CommonFeatures.tuple, new Values.Var(), new ArrayList<Value>(queue)));
+				} else {
+					rewritePoly(degree - 1, i + 1, args, dest, queue);
+				}
+				queue.removeLast();
+			}
+		}
+		
+
+		@Override
+		public Feature rewrite(Feature f) {
+			Deque<Value> q = new ArrayDeque<Value>(f.args());
+			List<Value> args = new ArrayList<Value>();
+			
+			while(!q.isEmpty()) {
+				Value v = q.pollFirst();
+
+				if((v instanceof FeatureParser.Special)) {
+					FeatureParser.Special feat = (FeatureParser.Special)v;
+					if(feat.token.equals("poly"))
+						rewritePoly(feat, args);
+					else if(feat.token.equals("poly#"))
+						rewritePolyExtended(feat, args);
+					else {
+						args.add(rewrite(feat));
+					}
+				} else {
+					if(v instanceof Feature)
+						v = rewrite((Feature)v);
+					args.add(v);
+				}
+			}
+			
+			f.setArgs(args);
+			return f;
+		}
 		
 	}
+	
 }
